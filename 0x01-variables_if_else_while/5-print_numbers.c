61fab964834f7f5b676713c5eafad00abb310870
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-12 17:42:10 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,21 +2,20 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Description: print 0-9 using putchar while using int variable
+ * Description: print 0-9 using putchar while using int variable
  *
- *Return: Always O (Success)
- */
-  int main(void)
-  {
-    int digit = 0;
-    while (digit <= 9)
-      {
-	printf("%i" , digit);
-        digit++;
-      }
-    printf("\n");
-    
-    return (0);
-  }
+ * Return: Always O (Success)
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
