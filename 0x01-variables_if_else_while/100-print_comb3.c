598e2c4367f3644b894b3fbbd0edf1703421f0c8
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-14 12:09:51 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,29 @@
-main - Entry point
-Description: biationbbrewo/sigitterent
-Return: Always O (Success)
-int main (void)
-int digit = 0, digit2:
-  while (digitl <n 9)
-    if (digita la digit2 de digiti < digita)
-      putchar (digitl
-	       48)
-	putchar (digit2 +
-		 48)
-	if (digitl + digita I-E
-putchar
-putchar
-	    }
-    digitar;
-digichtt:
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: biationbbrewo/sigitterent
+ *
+ * Return: Always O (Success)
+*/
+
+int main(void)
+{
+int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1 ; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+if (digit1 == 8 && digit2 == 9)
+continue;
+putchar(',');
+putchar(' ');
 }
-putchar("\n");
-return (0):
+}
+putchar('\n');
+return (0);
+}
