5a510fa03372ecff59b21892b179e217feedd893
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-15 18:49:23 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,18 @@ void times_table(void)
 int num, mult, prod;
 for (num = 0; num <= 9; num++)
 {
+_putchar(48);
+for (mult = 1; multi <= 9; multi++)
+{
 _putchar(',');
 _putchar(' ');
 prod = num * mult;
+if (prod <= 9)
 _putchar(' ');
 else
 _putchar((prod / 10) + 48);
 _putchar((prod % 10) + 48);
 }
 _putchar('\n')
+}
 }
