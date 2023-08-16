19aa94442d6ce30ba03346837a053d40e89fe73d
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-15 18:28:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,14 +6,16 @@
 void jack_bauer(void)
 {
 int hour, minute;
-for (hour = 0; hour <= 23; hour++
+for (hour = 0; hour <= 23; hour++)
 {
 for (minute = 0; minute <= 59; minute++)
+{
 _putchar((hour / 10) + 48);
 _putchar((hour % 10) + 48);
 _putchar(':');
 _putchar((minute / 10) + 48);
 _putchar((minute % 10) + 48);
 _putchar('\n');
+}
 }
 }
