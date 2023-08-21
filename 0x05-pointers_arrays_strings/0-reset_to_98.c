9352b1b7e0c1205b1c27a9e4e9dcd1655fb65ddc
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-21 13:24:01 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * rest_to_98 -Takes a pointer to an int and update the value it point to to 98
+ * @n: The pointer to an int
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putcar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar - writes the charactor c to stdout
+ * @c: The chractor to print
+ *
+ * Return: On success 1
+ *
+ * On error, -1 returned and error is set 
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
