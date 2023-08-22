8b2eff9a3dc89e5e85b1733998a563445270a99e
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-21 17:04:19 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,14 +5,14 @@
  */
 void rev_string(char *s)
 {
-int line, i;
+int line, i = 0;
 char tmp;
 for (line = 0; s[line] != '\0'; line++)
 ;
-for (i = 0; i < line / 2; i++)
+for (i = line - 1; i >= line / 2; i--)
 {
 tmp = s[i];
 s[i] = s[line - i - 1];
-s[line - i - 1] = s[i];
+s[line - i - 1] = tmp;
 }
 }
