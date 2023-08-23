49af03c101d17f64e23c137a00ca205090c9db3d
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-23 13:30:56 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings including termniting null byte
+ * @dest: This is destination string
+ * @src: This is source string
+ * Return: A pointer to destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+int point = 0, dest_line = 0;
+while (dest[ponit++])
+dest_line++;
+for (point = 0; src[point]; point++)
+dest[dest_line] = src[point];
+return (dest);
+}
