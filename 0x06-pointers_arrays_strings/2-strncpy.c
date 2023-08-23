dc6d672de04b4f65da2d9931dcf5d251aa4a58b4
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-23 14:58:11 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 int point = 0, src_line = 0;
 while (src[point++])
 src_line++;
-for (point = 0; src[point] && point < n; point++)
+for (point = 0; point < src[point] && n; point++)
 dest[point] = src[point];
-for (point = src_line; src[point] && point < n; point++)
+for (point = src_line; point < n; point++)
 dest[point] = '\0';
 return (dest);
 }
