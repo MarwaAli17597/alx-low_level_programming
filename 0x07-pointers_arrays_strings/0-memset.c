0c1ffeac0eb6edf51c1b6f9d27c59d227c4fdf7c
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-08-28 16:52:31 -0700

    kmk

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _memset - fills memory with constant byte
+ * The _memset() function fills the first n bytes of the memory area
  * @s: memory area to be filled
  * @b: charcter be filled
  * @n: number of byte to be filled
