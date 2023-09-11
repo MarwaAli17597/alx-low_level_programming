55222e948bfe1de00a9ab0c08419c6bba06a4e6b
MarwaAli17597 <Marwa.m.atta97@gmail.com>
2023-09-11 12:36:30 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,5 +16,5 @@ if (d->name == NULL)
 d->name = NILL;
 if (d->owner == NULL)
 d->owner = NILL;
-printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name,d->age, d->owner);
+printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
